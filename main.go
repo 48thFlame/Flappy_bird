c758a38e0067ef34a076c0d8d5ca2e974826aad9
avitar64 <avishai@taragin.com>
2022-03-10 15:51:56 +0200

    Hello World window

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+
+	pix "github.com/faiface/pixel"
+	pixgl "github.com/faiface/pixel/pixelgl"
+)
+
+const (
+	WindowWidth  = 1024
+	WindowHeight = 768
+)
+
 func main() {
-	println("Hello, World!")
+	pixgl.Run(run)
+}
+
+func run() {
+	fmt.Println("Hello, World!")
+
+	winConf := pixgl.WindowConfig{
+		Title:  "Hello, World!",
+		Bounds: pix.R(0, 0, WindowWidth, WindowHeight),
+	}
+
+	win, err := pixgl.NewWindow(winConf)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
 }
